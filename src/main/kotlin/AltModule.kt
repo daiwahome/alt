a99c2550d1c484fc7d9ea78ebfbfe44c7e6faa46
daiwahome <daiwahome0@gmail.com>
2019-09-27 23:01:59 +0900

    Add GoogleApiService

--- src/main/kotlin/AltModule.kt
+++ src/main/kotlin/AltModule.kt
@@ -1,10 +1,30 @@
 package com.github.daiwahome.alt
 
 import com.github.daiwahome.alt.cli.HelloCli
+import com.github.daiwahome.alt.service.FilterService
+import com.github.daiwahome.alt.service.GoogleApiService
 import com.github.daiwahome.alt.service.HelloService
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
 import org.koin.dsl.module
 
 val altModule = module {
+    // CLI
     single { HelloCli(get()) }
+
+    // Service
     single { HelloService() }
+    single { GoogleApiService(get()) }
+    single { FilterService() }
+
+    // Other
+    single {
+        HttpClient(OkHttp) {
+            install(JsonFeature) {
+                serializer = KotlinxSerializer()
+            }
+        }
+    }
 }
---
+++ src/main/kotlin/service/GoogleApiService.kt
@@ -0,0 +1,38 @@
+package com.github.daiwahome.alt.service
+
+import com.github.daiwahome.alt.model.ReverseGeocodingResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.http.URLBuilder
+import kotlinx.coroutines.runBlocking
+
+class GoogleApiService(
+    private val client: HttpClient
+) {
+
+    companion object {
+
+        private const val BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
+    }
+
+    /**
+     * Obtain addresses from Google maps API.
+     *
+     * @param latitude
+     * @param longitude
+     * @return response
+     */
+    fun requestReverseGeocoding(
+        latitude: Double,
+        longitude: Double,
+        apiKey: String
+    ): ReverseGeocodingResponse = runBlocking {
+        val url = URLBuilder(BASE_URL).apply {
+            parameters.apply {
+                append("latlng", "$latitude,$longitude")
+                append("key", apiKey)
+            }
+        }.build()
+        client.use { it.get<ReverseGeocodingResponse>(url) }
+    }
+}
