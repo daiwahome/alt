9603e6d6cd23eef768213a4530b94b318d903114
daiwahome <daiwahome0@gmail.com>
2019-09-28 12:12:23 +0900

    Add AltCli

--- src/main/kotlin/AltApplication.kt
+++ src/main/kotlin/AltApplication.kt
@@ -1,20 +1,25 @@
 package com.github.daiwahome.alt
 
-import com.github.daiwahome.alt.cli.HelloCli
+import com.github.daiwahome.alt.cli.AltCli
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.core.inject
 
+@kotlinx.serialization.UnstableDefault
 class AltApplication : KoinComponent {
 
-    val helloCli: HelloCli by inject()
+    val altCli: AltCli by inject()
 }
 
+@kotlinx.serialization.UnstableDefault
 fun main(args: Array<String>) {
     startKoin {
         modules(altModule)
     }
 
     val application = AltApplication()
-    application.helloCli.main(args)
+    application.altCli.main(args)
+
+    stopKoin()
 }
