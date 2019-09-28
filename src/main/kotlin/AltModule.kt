98b784107717d88f9a5f3e7966f01e5cb830648c
daiwahome <daiwahome0@gmail.com>
2019-09-28 16:18:52 +0900

    Use default locale language

--- src/main/kotlin/AltModule.kt
+++ src/main/kotlin/AltModule.kt
@@ -5,9 +5,12 @@ import com.github.daiwahome.alt.service.FilterService
 import com.github.daiwahome.alt.service.GoogleApiService
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.defaultRequest
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.header
 import org.koin.dsl.module
+import java.util.Locale
 
 @kotlinx.serialization.UnstableDefault
 val altModule = module {
@@ -23,6 +26,9 @@ val altModule = module {
         HttpClient(Apache) {
             install(JsonFeature) {
                 serializer = KotlinxSerializer()
+            }
+            defaultRequest {
+                header("Accept-Language", Locale.getDefault().language)
             }
         }
     }
